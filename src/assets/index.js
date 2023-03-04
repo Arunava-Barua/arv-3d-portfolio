605f6f8e9527689ba4ec2186918222d7404070fa
Arunava-Barua <rahulbarua31@gmail.com>
2023-03-04 17:25:32 +0530

    Portfolio done

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import ethereum from "./tech/Ethereum.png";
+import ethereum from "./tech/ethereum.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
